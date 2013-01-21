039575a11fc1bd292504fb887eb2e618918033db
James Smith <james@floppy.org.uk>
2013-01-21 07:44:42 +0000

    Add rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClimateStats::Application.config.secret_token = '4409b0d8b312c8bf843917bded24c5650e3b82de5905db78cacdd9c85d70a3221d902a11bc2c54e3693b7f749549d7c7aa3091465e66d6abd1d2f50edb22286d'
