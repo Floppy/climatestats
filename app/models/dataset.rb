72c20d22c98bae31afe71c71998ec5f64c01b074
James Smith <james@floppy.org.uk>
2013-01-21 08:26:56 +0000

    Move old script code into new models

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -2,3 +2,20 @@ class Dataset < ActiveRecord::Base
   attr_accessible :compare_to, :data_column, :data_uri, :fullname, :info_uri, :month_column, :shortname, :units, :year_column
   has_many :measurements
 end
+
+def update
+  # Get data
+  logger.info "Dataset: fetching data for #{shortname}"
+  rawdata = open(data_uri).read
+  # Parse datafile
+  logger.info "Dataset: parsing data"
+  CSV::Reader.parse(rawdata, ' ') do |row|
+    # Dump comments
+    unless row[0] == '#'
+      # Dump empty fields
+      row.delete_if { |item| item.nil? }
+      # Create new measurement
+      Measurement.create(:measured_on => Date.new(row[year_column], row[month_column]), :value => row[data_column.to_f])
+    end
+  end
+end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,31 @@
 class Measurement < ActiveRecord::Base
   belongs_to :dataset
   attr_accessible :measured_on, :value
+  validates :measured_on, :uniqueness, :scope => [:dataset]
+  
+  after_create :tweet
+
+  def measurement_for_comparison
+    dataset.measurements.where(:measured_on => measured_on - dataset.compare_to.months).first
+  end
+  
+  def textual_description
+    # Create tweet
+    text = "#{dataset.fullname} in #{measured_on.strftime("%B %Y")} were #{value}#{dataset.units}"
+    # Add comparison string        
+    previous = measurement_for_comparison
+    if measurement_for_comparison
+      difference = value - previous.value
+      direction = difference < 0 ? "down" : "up";
+      percentage = (difference.abs / previous.value) * 100
+      text += ", #{direction} #{sprintf("%.2f", difference)}#{dataset.units} (#{sprintf("%.1f", percentage)}%) from #{dataset.compare_to.months} months earlier."
+    end
+  end
+  
+  def tweet
+    logger.info "Measurement: tweeting..."
+    # Post
+    Twitter.update(textual_description)
+  end
+
 end
