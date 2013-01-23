04d3d87ffa1b18dd2c8f6d0d905287391416bab8
James Smith <james@floppy.org.uk>
2013-01-23 16:47:04 +0000

    Add tests for parser

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -1,21 +1,25 @@
+require 'open-uri'
+require 'csv'
+
 class Dataset < ActiveRecord::Base
   attr_accessible :compare_to, :data_column, :data_uri, :fullname, :info_uri, :month_column, :shortname, :units, :year_column
   has_many :measurements, :order => :measured_on
-end
-
-def update
-  # Get data
-  logger.info "Dataset: fetching data for #{shortname}"
-  rawdata = open(data_uri).read
-  # Parse datafile
-  logger.info "Dataset: parsing data"
-  CSV::Reader.parse(rawdata, ' ') do |row|
-    # Dump comments
-    unless row[0] == '#'
-      # Dump empty fields
-      row.delete_if { |item| item.nil? }
-      # Create new measurement
-      Measurement.create(:measured_on => Date.new(row[year_column], row[month_column]), :value => row[data_column.to_f])
+  
+  def update
+    # Get data
+    logger.info "Dataset: fetching data for #{shortname}"
+    rawdata = open(data_uri).read
+    # Parse datafile
+    logger.info "Dataset: parsing data"
+    CSV.parse(rawdata, :col_sep => ' ') do |row|
+      # Dump comments
+      unless row[0] == '#'
+        # Dump empty fields
+        row.delete_if { |item| item.nil? }
+        # Create new measurement
+        measurements.create(:measured_on => Date.new(row[year_column].to_i, row[month_column].to_i), :value => row[data_column.to_f])
+      end
     end
   end
+  
 end
--- spec/models/dataset_spec.rb
+++ spec/models/dataset_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe Dataset do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context 'importing new data' do
+    
+    before :all do
+      #DatabaseCleaner.start
+      @dataset = FactoryGirl.create :dataset
+    end
+    
+    it 'should import data from file correctly' do
+      # Set up webmock stub so we don't really do a fetch
+      @dataset.should_receive(:open).with("ftp://ftp.cmdl.noaa.gov/ccg/co2/trends/co2_mm_gl.txt").and_return(File.open(File.join(Rails.root, 'spec', 'fixtures', 'co2_mm_gl.txt')))
+      # Import
+      @dataset.update
+      # Check measurements
+      @dataset.measurements.count.should == 23
+      @dataset.measurements.first.measured_on.should == Date.new(2011,1)
+      @dataset.measurements.first.value.should == 390.74
+      @dataset.measurements.last.measured_on.should == Date.new(2012,11)
+      @dataset.measurements.last.value.should == 393.65
+      
+    end
+    
+    after :all do
+      # => DatabaseCleaner.clean
+    end
+
+  end
+
 end
